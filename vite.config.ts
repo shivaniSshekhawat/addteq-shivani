6c154b005c2c4126b3e65239cafeab4c24eeb50f
shivaniSshekhawat <shivanishekhawat2694@gmail.com>
2025-07-18 00:45:19 +0530

    *update

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,12 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/addteq-shivani/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
